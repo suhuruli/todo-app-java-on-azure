70328811a304444b16da17532bf95088cdf9ff62
Sudhanva Huruli <sudhanvahuruli@gmail.com>
2018-09-24 09:43:56 -0400

    working RC demo

--- entrypoint.bat
+++ entrypoint.bat
@@ -6,6 +6,6 @@ set PATH=C:\sffabricbin;C:\Code;%PATH%
 C:\SFFabricBin\vc14_redist.x64.exe -quiet
 C:\SFFabricBin\vcredist_x64.exe -quiet
 set > env.log
-java -Djava.library.path=%PATH% -jar todo-app-java-on-azure-1.0-SNAPSHOT.jar  >>  javaprocess.log
+java -Djava.library.path=%PATH% -jar todo-app-java-on-azure-1.0-SNAPSHOT-RC-DEMO-1.jar  >>  javaprocess.log
 
 start /wait timeout 600
